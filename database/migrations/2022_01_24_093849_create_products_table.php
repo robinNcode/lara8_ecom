a77f3f7df586146d699900aa3062038e803bb0f4
robinNcode <msmrobin518@gmail.com>
2022-01-24 16:16:13 +0600

    migration done

--- database/migrations/2022_01_24_093849_create_products_table.php
+++ database/migrations/2022_01_24_093849_create_products_table.php
@@ -14,7 +14,12 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('detail');
+            $table->double('price');
+            $table->double('stock');
+            $table->double('discount');
             $table->timestamps();
         });
     }
